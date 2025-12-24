e07729d68adf454cf182004e56634530da3f712c
JUSTCHILL098 <sandy2761910@gmail.com>
2025-12-24 17:16:07 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./app/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        mono: ["var(--font-geist-mono)"],
+      },
+      borderRadius: {
+        lg: "0.75rem",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
